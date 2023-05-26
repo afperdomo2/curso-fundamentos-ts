c3e355de42d7ba18d0f27da1116001b98a0e6a0f
afperdomo2 <alvarado0925@gmail.com>
2023-05-26 15:16:18 -0500

    Implementa ts-check para hacer verificación de tipo estático al archivo demo.js

--- src/demo.js
+++ src/demo.js
@@ -1,37 +1,50 @@
-(()=> {
+/**
+ * Cuando se utiliza ts-check en un archivo JavaScript, se le indica al
+ * compilador de TypeScript que realice una verificación de tipo estático
+ * en ese archivo, incluso si el archivo no tiene una extensión .ts o .tsx.
+ * Esto permite aprovechar las capacidades de verificación de tipos de
+ * TypeScript en archivos JavaScript existentes, sin necesidad de convertirlos
+ * completamente a TypeScript.
+ */
+
+//@ts-check
+
+() => {
   const myCart = [];
   const products = [];
   const limit = 2;
 
   async function getProducts() {
-    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
-      mehtod: 'GET'
+    const rta = await fetch("http://api.escuelajs.co/api/v1/products", {
+      method: "GET",
     });
-    const data = await rta.parseJson();
+    const data = await rta.json();
     products.concat(data);
   }
+
   function getTotal() {
-    const total = 0;
-    for (const i = 0; i < products.length(); i++) {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
       total += products[i].prize;
     }
     return total;
   }
+
   function addProduct(index) {
-    if (getTotal <= limit) {
+    if (getTotal() <= limit) {
       myCart.push(products[index]);
     }
   }
 
-  await getProducts();
-  addProducto(1);
-  addProducto(2);
+  getProducts();
+  addProduct(1);
+  addProduct(2);
   const total = getTotal();
-  console.log(total);
+  console.info(total);
   const person = {
-    name: 'Nicolas',
-    lastName: 'Molina'
-  }
-  const rta = person +  limit;
-  console.log(rta);
-});
+    name: "Nicolas",
+    lastName: "Molina",
+  };
+  const rta = `${person}  ${limit}`;
+  console.info(rta);
+};
