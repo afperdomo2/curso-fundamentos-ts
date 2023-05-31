74a46d9ef1b567c43b4434e351baa34dcf019da0
afperdomo2 <alvarado0925@gmail.com>
2023-05-31 17:43:11 -0500

    Usando el tipado de datos en Strings

---
+++ src/06-strings.ts
@@ -0,0 +1,9 @@
+// Tipado de Strings
+(() => {
+  // Inferido
+  let nombre = 'Pepito';
+
+  let fullName: string = 'Pepito Perez';
+  fullName = 'Margarita Fernandez';
+  console.log('fullName:', fullName);
+})();
