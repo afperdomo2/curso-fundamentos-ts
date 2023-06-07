7188c165153faeab643907deb1562072c0e768d9
afperdomo2 <alvarado0925@gmail.com>
2023-06-07 15:36:37 -0500

    Ajustes menores en el archivo de tipado de objetos

--- src/14-object-typing.ts
+++ src/14-object-typing.ts
@@ -14,15 +14,20 @@
   type Product = { title: string; stock: number; size?: Sizes };
 
   // Array de objetos de tipo: 'Product'
+  // Las 2 siguientes formas son equivalentes
   const products: Array<Product> = [];
+  const products2: Product[] = [];
 
   // Agrega el tipado de 'Product' al argumento de 'producto'
   const addProduct = (product: Product) => {
     products.push(product);
+    products2.push(product);
   };
   addProduct({ title: 'Pantalon', stock: 12, size: 'L' });
   addProduct({ title: 'Camisa', stock: 3 });
   addProduct({ title: 'Cinturón', stock: 4 });
   addProduct({ title: 'Medias', stock: 4, size: 'One Size' });
+
   console.table(products);
+  console.table(products2);
 })();
